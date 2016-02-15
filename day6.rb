ec34e419f949efec4962783778b6b8f116d4af08
Derreck Mansheim <derreckmansheim@Derrecks-MacBook-Pro.local>
2016-02-15 17:54:10 -0600

    Reflections aggregated from around the computer

---
+++ day6.rb
@@ -0,0 +1,21 @@
+#Explain how && works (what are the rules for figuring out what it will do?)
+All sides must evaluate to true, else false.
+
+
+
+#Explain how || works (include an example and walk through how Ruby evaluates
+#the example)
+Only one side need evaluate to true to be true, else false.
+
+
+
+
+
+#What is the difference between if and while?
+
+
+
+
+
+#What is the difference between unless and until?
+
